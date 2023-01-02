a427abebcec22e70b8e809c0f27f246f66b3e4bb
Dinil Kumarasiri <64276626+DinilKumarasiri@users.noreply.github.com>
2023-01-02 22:47:53 +0530

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -5,16 +5,16 @@
 	$subject = $_POST['subject'];
 	$message = $_POST['message'];
 
-	$email_form = 'DN tech';
+	$email_form = 'Alene Soft';
 	
-	$email_subject = 'Contact Buyer Request';
+	$email_subject = 'Contact Client Request';
 
 	$email_body = "User Name : $name.\n".
 			"User Email: $visitor_email. \n".
 			"Subject : $subject. \n". 
 			"User Message : $message \n";
 			
-	$to = 'dinilkumarasiri@hdnktech.lk';	
+	$to = 'contact.us@alenesoft.com';	
 
 	$headers = "Form : $email_form \r\n";
 
