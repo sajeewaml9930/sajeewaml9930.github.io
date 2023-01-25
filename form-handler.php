d272bec6d9e4ddeb02b2d92896b06c350723729d
sajeewa9930 <sajeewaml@gmail.com>
2023-01-25 22:04:43 +0530

    new

---
+++ form-handler.php
@@ -0,0 +1,28 @@
+<?php 
+
+	$name = $_POST['name'];
+	$visitor_email = $_POST['email'];
+	$subject = $_POST['subject'];
+	$contact = $_POST['contact'];
+	$message = $_POST['message'];
+
+	$email_form = 'Alene Soft';
+	
+	$email_subject = 'Contact Client Request';
+
+	$email_body = "User Name : $name.\n".
+			"User Email: $visitor_email. \n".
+			"Subject : $subject. \n". 
+			"ContactNo : $contact. \n". 
+			"User Message : $message \n";
+			
+	$to = 'contact.us@alenesoft.com';	
+
+	$headers = "Form : $email_form \r\n";
+
+	$headers .= "Reply-To : $visitor_email \r\n";
+
+	mail($to,$email_subject,$email_body,$headers);
+
+	header("Location :contact-us.html");
+?>
