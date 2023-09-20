9b74f90a77d0e23e507bbd23f72bd5de96a7cc3e
sajeewa9930 <sajeewaml@gmail.com>
2023-09-20 17:05:26 +0530

    new

--- js/javascript.js
+++ js/javascript.js
@@ -46,4 +46,27 @@ $(document).ready(function() {
               $("#navigation").removeClass("navbar-color");
           }
   })
+  $(".customer-logos").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 4
+        }
+      },
+      {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 3
+        }
+      }
+    ]
+  });
 });
